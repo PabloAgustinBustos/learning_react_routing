ce0953517d799311b5174ea9a7b31dd339aaca36
Pablo <pablobustos609@gmail.com>
2022-09-18 08:28:44 -0300

    link learned

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import About from './pages/About';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import Error from './pages/Error';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <nav>the navbar</nav>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="about" element={<About/>}/>
           <Route path="products" element={<Products/>}/>
+          <Route path="*" element={<Error/>}/>
         </Routes>
       </div>
     </BrowserRouter>
